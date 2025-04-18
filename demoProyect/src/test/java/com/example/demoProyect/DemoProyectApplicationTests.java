e776fda26385c9e5cb65b50c2df3dad023ab933b
d03f <doef.proyects@gmail.com>
2025-04-18 12:06:29 +0200

    Basic proyect structure

---
+++ demoProyect/src/test/java/com/example/demoProyect/DemoProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
