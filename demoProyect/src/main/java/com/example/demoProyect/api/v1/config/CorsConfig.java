44928497546af43e8e18764ec228259eb83608a5
d03f <doef.proyects@gmail.com>
2025-05-18 14:31:06 +0200

    Fixed changes. Moved website to another repo

---
+++ demoProyect/src/main/java/com/example/demoProyect/api/v1/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.example.demoProyect.api.v1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply CORS to all endpoints
+                .allowedOrigins("*") // Allow requests from any origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(false)
+                .maxAge(3600); // Cache the profiled response for 1 hour
+    }
+}
